dd39637e6218eab0dc848a1089aca03e46729d96
MNygren <mattnygren_7@hotmail.com>
2017-08-21 10:36:12 -0700

    Add project files.

---
+++ EIS.DAL/EISDBContext.cs
@@ -0,0 +1,25 @@
+﻿using EIS.BOL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIS.DAL
+{
+    public class EISDBContext : DbContext
+    {
+        public EISDBContext():base("EISDB3")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<EISDBContext, EIS.DAL.Migrations.Configuration>());
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+     
+    }
+}
---
+++ MattConsoleUnitTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using EIS.BOL;
+using EIS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MattConsoleUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoleDb R = new RoleDb();
+            R.Insert(new Role() { RoleName = "Admin", RoleCode = "A" });
+            R.Save();
+
+        }
+    }
+}
